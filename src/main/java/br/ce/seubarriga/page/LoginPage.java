de7c9b370231cae497e34d452d58ff9661e4fd6a
Luiz Eduardo <luiz.eduardo@boavista.local>
2021-04-19 01:38:04 -0300

    Ajustando padrão do método

--- src/main/java/br/ce/seubarriga/page/LoginPage.java
+++ src/main/java/br/ce/seubarriga/page/LoginPage.java
@@ -18,13 +18,13 @@ public class LoginPage extends BasePage {
 	public void setSenha(String senha) {
 		escrever("senha", senha);
 	}
-	public void btnEntrar() {
+	public void botaoEntrar() {
 		clicar(By.xpath("//button[.='Entrar']"));
 	}
 	public void logar(String email, String senha) {
 		setEmail(email);
 		setSenha(senha);
-		btnEntrar();
+		botaoEntrar();
 	}
 	
 }
